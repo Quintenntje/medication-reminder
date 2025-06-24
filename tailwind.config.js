8f103f222dc01154e994b9f8f67126b67564be4f
Quinten <quintenclaes7@gmail.com>
2025-06-24 22:51:01 +0200

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#2563EB", 
+          light: "#3B82F6", 
+          dark: "#1D4ED8", 
+        },
+        secondary: {
+          DEFAULT: "#14B8A6", 
+          dark: "#0F766E",
+        },
+        background: {
+          DEFAULT: "#F8FAFC", 
+        },
+        surface: "#ffffff",
+        accent: "#0EA5E9", 
+        success: "#22C55E", 
+        warning: "#F59E0B", 
+        text: {
+          DEFAULT: "#1E293B", 
+          light: "#F1F5F9",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
