187d54d29bb14b59c34899dd2832fad96d499ffa
Quinten <quintenclaes7@gmail.com>
2025-06-25 22:15:10 +0200

    feat: align layout and spacing in Medication and Settings components

--- app/(tabs)/medication.tsx
+++ app/(tabs)/medication.tsx
@@ -3,15 +3,14 @@ import { Text, useColorScheme, View } from "react-native";
 
 const Medication = () => {
   const colorScheme = useColorScheme();
-
   return (
     <View
-      className={`flex-1 items-center justify-center ${
+      className={`flex-1 ${
         colorScheme === "dark" ? "bg-gray-900" : "bg-white"
       }`}
     >
       <Text
-        className={`text-2xl font-semibold ${
+        className={`text-2xl font-semibold mt-20 mx-4 ${
           colorScheme === "dark" ? "text-white" : "text-gray-900"
         }`}
       >
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -6,12 +6,12 @@ const Settings = () => {
 
   return (
     <View
-      className={`flex-1 items-center justify-center ${
+      className={`flex-1  ${
         colorScheme === "dark" ? "bg-gray-900" : "bg-white"
       }`}
     >
       <Text
-        className={`text-2xl font-semibold ${
+        className={`text-2xl font-semibold mt-20 mx-4 ${
           colorScheme === "dark" ? "text-white" : "text-gray-900"
         }`}
       >
