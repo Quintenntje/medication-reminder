f8a24bab8badafcf96a2588f8ee0f7dccc574661
Quinten <quintenclaes7@gmail.com>
2025-06-25 22:35:22 +0200

    feat: update Medication and Settings components with improved layout and icon integration

--- app/(tabs)/medication.tsx
+++ app/(tabs)/medication.tsx
@@ -9,13 +9,26 @@ const Medication = () => {
         colorScheme === "dark" ? "bg-gray-900" : "bg-white"
       }`}
     >
-      <Text
-        className={`text-2xl font-semibold mt-20 mx-4 ${
-          colorScheme === "dark" ? "text-white" : "text-gray-900"
-        }`}
-      >
-        Medication
-      </Text>
+      <View className="p-4 mt-20">
+        <Text
+          className={`text-4xl font-semibold  ${
+            colorScheme === "dark" ? "text-white" : "text-gray-900"
+          }`}
+        >
+          Medication
+        </Text>
+
+        <View className="mt-8">
+          <Text
+            className={`text-lg mt-4 mx-4 ${
+              colorScheme === "dark" ? "text-gray-300" : "text-gray-700"
+            }`}
+          >
+            Manage your medications here. Add, edit, or remove medications as
+            needed.
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, useColorScheme, View } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const Settings = () => {
   const colorScheme = useColorScheme();
@@ -10,13 +11,42 @@ const Settings = () => {
         colorScheme === "dark" ? "bg-gray-900" : "bg-white"
       }`}
     >
-      <Text
-        className={`text-2xl font-semibold mt-20 mx-4 ${
-          colorScheme === "dark" ? "text-white" : "text-gray-900"
-        }`}
-      >
-        Settings
-      </Text>
+      <View className="p-4 mt-20">
+        <Text
+          className={`text-4xl font-semibold   ${
+            colorScheme === "dark" ? "text-white" : "text-gray-900"
+          }`}
+        >
+          Settings
+        </Text>
+        <View className="mt-12">
+          <View
+            className={`flex-row items-center justify-between w-full p-4 border-t border-b ${
+              colorScheme === "dark" ? "border-gray-700" : "border-gray-200"
+            }`}
+          >
+            <View className="flex-row items-center">
+              <Icon
+                name="lock-closed-outline"
+                size={28}
+                color={colorScheme === "dark" ? "#D1D5DB" : "#374151"}
+              />
+              <Text
+                className={`text-2xl ml-3 ${
+                  colorScheme === "dark" ? "text-gray-300" : "text-gray-700"
+                }`}
+              >
+                Security Options
+              </Text>
+            </View>
+            <Icon
+              name="chevron-forward-outline"
+              size={28}
+              color={colorScheme === "dark" ? "#9CA3AF" : "#6B7280"}
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
