a4c28728ada66e5b797388c23a854715e0202c1f
Quinten <quintenclaes7@gmail.com>
2025-06-25 21:57:12 +0200

    feat: enhance tab navigation with icons and update dependencies

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,43 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#007AFF",
+        tabBarInactiveTintColor: "#8E8E93",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Calendar",
           headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="calendar-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="medication"
+        options={{
+          title: "Medication",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="medical-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="settings-outline" size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
